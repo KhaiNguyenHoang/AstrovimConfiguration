d095e01ef0701e9029657522b48a322bf0f89fd8
Nguyen Hoang Khai <khaiproject1234@gmail.com>
2025-11-25 01:11:53 +0700

    Update

--- lua/plugins/Fknotes.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "flashcodes-themayankjha/Fknotes.nvim",
-  dependencies = { "MunifTanjim/nui.nvim" },
-  config = function()
-    require("fknotes").setup {
-      -- your configuration here
-    }
-  end,
-}
